aa607fb3a3a217bfa782801c3bdaaa5079734c13
vijay-kumar-solanki <vijaythakor682003@gmail.com>
2023-09-28 16:34:52 +0530

    first

---
+++ routes/auth.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const authenticateUser = require("../middleware/authentication");
+const { register, login, updateUser } = require("../controllers/auth");
+const rateLimiter = require("express-rate-limit");
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 1,
+  message: {
+    msg: "Too many request from This IP please try again after 15 minutes",
+  },
+});
+router.post("/register", apiLimiter, register);
+router.post("/login", apiLimiter, login);
+router.patch("/updateUser", authenticateUser, updateUser);
+module.exports = router;
